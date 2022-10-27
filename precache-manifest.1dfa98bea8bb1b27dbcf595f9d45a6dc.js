37ff92e342c6879648e5c54d8f37d1c427985925
BenedictBenny <benedictbennybinu@gmail.com>
2022-10-28 00:42:13 +0530

    Updates

---
+++ precache-manifest.1dfa98bea8bb1b27dbcf595f9d45a6dc.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6db8d7f754bb8c2af8ca83170693902",
+    "url": "/DietPlanetMenu/index.html"
+  },
+  {
+    "revision": "0dafdd5181f3285b8bb7",
+    "url": "/DietPlanetMenu/static/css/main.01c2a192.chunk.css"
+  },
+  {
+    "revision": "d9477fddb5fe81a6c7ef",
+    "url": "/DietPlanetMenu/static/js/2.5c485663.chunk.js"
+  },
+  {
+    "revision": "a9e6b704516e0051376a8ceda0c6064b",
+    "url": "/DietPlanetMenu/static/js/2.5c485663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dafdd5181f3285b8bb7",
+    "url": "/DietPlanetMenu/static/js/main.0aa6156e.chunk.js"
+  },
+  {
+    "revision": "a50858d37442f95ea8ae",
+    "url": "/DietPlanetMenu/static/js/runtime-main.abb42621.js"
+  },
+  {
+    "revision": "bca47b03d5e2c19f59dafb3ddc0f876a",
+    "url": "/DietPlanetMenu/static/media/BananaNutsYogurt.bca47b03.jpg"
+  },
+  {
+    "revision": "495f945730d2f197617c787584621d92",
+    "url": "/DietPlanetMenu/static/media/BananaPancake.495f9457.jpg"
+  },
+  {
+    "revision": "d3043b61547ee7a842e37570c6784711",
+    "url": "/DietPlanetMenu/static/media/BeansChicken.d3043b61.jpg"
+  },
+  {
+    "revision": "7e5668428a42b4f568f4cbd299dda8fb",
+    "url": "/DietPlanetMenu/static/media/BoiledEgg.7e566842.jpg"
+  },
+  {
+    "revision": "68c543e66db42a1c009d06906dbe1661",
+    "url": "/DietPlanetMenu/static/media/BroccoliSalad.68c543e6.jpg"
+  },
+  {
+    "revision": "0f234dfad125ed739b65ce31f3f4b2d9",
+    "url": "/DietPlanetMenu/static/media/ChickenRiceCake.0f234dfa.jpg"
+  },
+  {
+    "revision": "21d46d5fd4f6a4a300ce6e50af6dfcb7",
+    "url": "/DietPlanetMenu/static/media/ChocolateNutsYogurt.21d46d5f.jpg"
+  },
+  {
+    "revision": "939e604bb99ef33d0c45d752dbac439f",
+    "url": "/DietPlanetMenu/static/media/ChocolatePancake1.939e604b.jpg"
+  },
+  {
+    "revision": "a7f681b48e03fd712860a48ed217e254",
+    "url": "/DietPlanetMenu/static/media/ChocolatePancake2.a7f681b4.jpg"
+  },
+  {
+    "revision": "d1e6abd2d1bd854ec76c796240c72ce0",
+    "url": "/DietPlanetMenu/static/media/ChocolateRiceCake.d1e6abd2.jpg"
+  },
+  {
+    "revision": "7a317a31e1ab3b61c0a645910cf4e3e2",
+    "url": "/DietPlanetMenu/static/media/CornSalad.7a317a31.jpg"
+  },
+  {
+    "revision": "d942ecb827bb78ac9fcf3b19e5f85fef",
+    "url": "/DietPlanetMenu/static/media/DarkOatsYogurt.d942ecb8.jpg"
+  },
+  {
+    "revision": "7d85a5e3fa75acfe201fee6bf45fce6b",
+    "url": "/DietPlanetMenu/static/media/DietPlanetLogo.7d85a5e3.png"
+  },
+  {
+    "revision": "5690e8f3d4b685ce8fec3f67a528a9b3",
+    "url": "/DietPlanetMenu/static/media/FruitBowlHoney.5690e8f3.jpg"
+  },
+  {
+    "revision": "105b1419e47ed878519d66f45fbb463a",
+    "url": "/DietPlanetMenu/static/media/FruitBowlSmoothie.105b1419.jpg"
+  },
+  {
+    "revision": "c239efad7f2d12164cc0fa9690cce68a",
+    "url": "/DietPlanetMenu/static/media/FruitSalad1.c239efad.jpg"
+  },
+  {
+    "revision": "2350a0229bda4a837efdf6c22db4fc96",
+    "url": "/DietPlanetMenu/static/media/FruitSalad2.2350a022.jpg"
+  },
+  {
+    "revision": "d5767b308a70f20fe5a0bbd65ee55e5a",
+    "url": "/DietPlanetMenu/static/media/FruitSalad3.d5767b30.jpg"
+  },
+  {
+    "revision": "fcf7e5eb4d969e864c1489567d6eb364",
+    "url": "/DietPlanetMenu/static/media/FruitSalad4.fcf7e5eb.jpg"
+  },
+  {
+    "revision": "810031cb95beb87c9bcebc25eb7c50da",
+    "url": "/DietPlanetMenu/static/media/FruitSalad5.810031cb.jpg"
+  },
+  {
+    "revision": "ced9f27ff8e6cea3afb558c140211e4b",
+    "url": "/DietPlanetMenu/static/media/FruitSalad6.ced9f27f.jpg"
+  },
+  {
+    "revision": "863c6c97088127369102d2b416ef8d4e",
+    "url": "/DietPlanetMenu/static/media/GrapeJuice.863c6c97.jpg"
+  },
+  {
+    "revision": "0d6474e199438dc3cb48728c0b12c3b3",
+    "url": "/DietPlanetMenu/static/media/GreenPeasPasta.0d6474e1.jpg"
+  },
+  {
+    "revision": "f62a440fce058befabb091b4d50bcfd4",
+    "url": "/DietPlanetMenu/static/media/MixedMeal1.f62a440f.jpg"
+  },
+  {
+    "revision": "c3b90ec75d05ca2accc611c79875b223",
+    "url": "/DietPlanetMenu/static/media/MixedMeals2.c3b90ec7.jpg"
+  },
+  {
+    "revision": "4cce05aed1002559c18669cc966d973b",
+    "url": "/DietPlanetMenu/static/media/NutsPancake.4cce05ae.jpg"
+  },
+  {
+    "revision": "cc2d5bf1a7c3b2fc983b018d8b88f496",
+    "url": "/DietPlanetMenu/static/media/NutsRiceCake.cc2d5bf1.jpg"
+  },
+  {
+    "revision": "d263cafba7349b94b62b0900fb3d01f4",
+    "url": "/DietPlanetMenu/static/media/OatsFruitAndNuts.d263cafb.jpg"
+  },
+  {
+    "revision": "956239f1ca4ecbe3dfeb3ff305cb035b",
+    "url": "/DietPlanetMenu/static/media/OatsKiwi.956239f1.jpg"
+  },
+  {
+    "revision": "4ab6bab6287aa2cb55de5afb7743c20c",
+    "url": "/DietPlanetMenu/static/media/OatsPeanutButterMuesli.4ab6bab6.jpg"
+  },
+  {
+    "revision": "3eecdea8b1b0ea55fe5114c7b760322b",
+    "url": "/DietPlanetMenu/static/media/Omelette1.3eecdea8.jpg"
+  },
+  {
+    "revision": "31478a587f280f8ef5eaeb659fe825b1",
+    "url": "/DietPlanetMenu/static/media/Omelette2.31478a58.jpg"
+  },
+  {
+    "revision": "3dc299bf09829169a69a078bdbec34a8",
+    "url": "/DietPlanetMenu/static/media/Omelette3.3dc299bf.jpg"
+  },
+  {
+    "revision": "bc6a1aba63dab03d32ffef668d4ab947",
+    "url": "/DietPlanetMenu/static/media/PaneerBroccolliMeal.bc6a1aba.jpg"
+  },
+  {
+    "revision": "e78e2c2494a2df7afca7911b04bb7a85",
+    "url": "/DietPlanetMenu/static/media/PaneerSalad.e78e2c24.jpg"
+  },
+  {
+    "revision": "068dc77ab8f80527de2b40525dc4b433",
+    "url": "/DietPlanetMenu/static/media/PeanutButterBananaRiceCake1.068dc77a.jpg"
+  },
+  {
+    "revision": "ca45aab510b456778273117d0b750890",
+    "url": "/DietPlanetMenu/static/media/PeanutButterBananaRiceCake2.ca45aab5.jpg"
+  },
+  {
+    "revision": "cd86479072bea32f9956cc283e55b2a2",
+    "url": "/DietPlanetMenu/static/media/PeanutButterBananaToast.cd864790.jpg"
+  },
+  {
+    "revision": "88fbaa5fd4c1c50d9e154be8fa7823c3",
+    "url": "/DietPlanetMenu/static/media/PineAppleGrilled.88fbaa5f.jpg"
+  },
+  {
+    "revision": "0ea267f15521ec1c78c638e8a9c00616",
+    "url": "/DietPlanetMenu/static/media/PineAppleJuice.0ea267f1.jpg"
+  },
+  {
+    "revision": "a5bebdc5d12c630aa36159cd5991974a",
+    "url": "/DietPlanetMenu/static/media/ProteinShake1.a5bebdc5.jpg"
+  },
+  {
+    "revision": "85df01ae34f0e02d1bc157b3cc7fd2c8",
+    "url": "/DietPlanetMenu/static/media/RiceBroccoliChickenMeal1.85df01ae.jpg"
+  },
+  {
+    "revision": "f5458e65f38dca35ce0ef6d4533a9981",
+    "url": "/DietPlanetMenu/static/media/RiceBroccoliChickenMeal2.f5458e65.jpg"
+  },
+  {
+    "revision": "984d37e20b1c06c2f2b23597b70a9fa8",
+    "url": "/DietPlanetMenu/static/media/RiceChickenMeal.984d37e2.jpg"
+  },
+  {
+    "revision": "2d37e28c1fd3f3414244eea22507dc4d",
+    "url": "/DietPlanetMenu/static/media/RiceSpinachEggMeal.2d37e28c.jpg"
+  },
+  {
+    "revision": "01436699bdc92b643b6b1d90c7483dec",
+    "url": "/DietPlanetMenu/static/media/SaucyPaneer.01436699.jpg"
+  },
+  {
+    "revision": "e57a013a4455ddb7247a09f62ea992f8",
+    "url": "/DietPlanetMenu/static/media/ScrambledEggs1.e57a013a.jpg"
+  },
+  {
+    "revision": "2c7366ef554c71dab95d48556bb7c0fe",
+    "url": "/DietPlanetMenu/static/media/ScrambledEggs2.2c7366ef.jpg"
+  },
+  {
+    "revision": "740d12fed3b24e505a3686340318e4f3",
+    "url": "/DietPlanetMenu/static/media/ScrambledEggsToast.740d12fe.jpg"
+  },
+  {
+    "revision": "f48b3cc9fe3d533a45bfe9e2cc4e8535",
+    "url": "/DietPlanetMenu/static/media/SpinachScrambledEggs.f48b3cc9.jpg"
+  },
+  {
+    "revision": "0e8be1bf0e7818129fd082717eab3677",
+    "url": "/DietPlanetMenu/static/media/SpinachScrambledEggsToast.0e8be1bf.jpg"
+  },
+  {
+    "revision": "75232e785b85c5c9bfda1d655f73744c",
+    "url": "/DietPlanetMenu/static/media/SproutedBeans.75232e78.jpg"
+  },
+  {
+    "revision": "7cb4d8abf3bef7498341fe2e79d0164f",
+    "url": "/DietPlanetMenu/static/media/SunnySideUp.7cb4d8ab.jpg"
+  },
+  {
+    "revision": "e069e5a370bb9e74d8245726446018af",
+    "url": "/DietPlanetMenu/static/media/VegSandwich1.e069e5a3.jpg"
+  },
+  {
+    "revision": "6d88fa2ab2815d8669c2dc9f6ed809e9",
+    "url": "/DietPlanetMenu/static/media/VegSandwich2.6d88fa2a.jpg"
+  },
+  {
+    "revision": "77f8623c7c8af03ab44090437ad9e1bb",
+    "url": "/DietPlanetMenu/static/media/VegSandwich3.77f8623c.jpg"
+  },
+  {
+    "revision": "b7de188086924b356bfe0a0ac5002ed3",
+    "url": "/DietPlanetMenu/static/media/WaterMelonFetaSalad.b7de1880.jpg"
+  },
+  {
+    "revision": "c2155e2074f07f8e5b60e9a972e96c1c",
+    "url": "/DietPlanetMenu/static/media/WatermelonJuice.c2155e20.jpg"
+  },
+  {
+    "revision": "617a4d01171467824eb2ce83055fa431",
+    "url": "/DietPlanetMenu/static/media/YogurtNutRiceCake.617a4d01.jpg"
+  }
+]);
